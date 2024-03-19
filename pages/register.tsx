1fe82fc53ec566e7e1c997a031bf49523d4e7e9a
adityasuman2025 <adityasuman2025@gmail.com>
2024-03-19 16:39:10 +0530

    next setup

---
+++ pages/register.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+export default function Register() {
+    useEffect(() => {
+        // if (window) window.location.replace("https://mngo.in");
+    }, []);
+
+    return (
+        <main className="mngo-bg-black">
+            MNgo Register
+        </main>
+    );
+}
